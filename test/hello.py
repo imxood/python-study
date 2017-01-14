1e161f3a773c62df3059dd47513cad2a392c7c8c
maxu <imxood@163.com>
2017-01-14 16:50:25 +0800

    学习tensorflow

--- test/hello.py
+++ test/hello.py
@@ -1,40 +1,40 @@
 import socket
 import sys
 
-print 'hello, python!'
+print('hello, python!')
+
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error, msg:
-    print 'failed to create socket. Error code: ' + str(msg[0]), 'Error message: ' + msg[1]
-    sys.exit();
+except socket.error as msg:
+    print('failed to create socket. Error code: ' + str(msg[0]), 'Error message: ' + msg[1])
+    sys.exit()
 
-print 'socket created!'
+print('socket created!')
 
 host = 'www.zhihu.com'
 port = 80
 
 try:
-    remote_ip = socket.gethostbyname( host )
+    remote_ip = socket.gethostbyname(host)
 except socket.gaierror:
-    print 'Hostname could not be resoled. Exiting.'
-    sys.exit();
-
-print 'Ip address of ' + host + ' is ' + remote_ip;
-
-s.connect((remote_ip , port))
- 
-print 'Socket Connected to ' + host + ' on ip ' + remote_ip
-
-message = "GET / HTTP/1.1\r\n\r\n"
-
-try:
-    s.sendall(message)
-except socket.error:
-    print 'Send failed'
+    print('Hostname could not be resoled. Exiting.')
     sys.exit()
 
-print 'Message send successfully'
+print('Ip address of ' + host + ' is ' + remote_ip)
+
+s.connect((remote_ip, port))
+ 
+print('Socket Connected to ' + host + ' on ip ' + remote_ip)
+
+try:
+    message = "GET / HTTP/1.1\r\n\r\n"
+    s.sendall(message)
+except socket.error as msg:
+    print(msg)
+    sys.exit()
+
+print('Message send successfully')
 
 reply = s.recv(4096)
 
-print reply
+print(reply)
